6329985cd8663c60cbad58bd13bfd220b743d192
8099bb4X21 <hydra1018@gmail.com>
2026-08-01 20:41:18 +0800

    feat: 添加哔哩哔哩直播规则

---
+++ src/apps/tv.danmaku.bili.ts
@@ -0,0 +1,16 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'tv.danmaku.bili',
+  name: '哔哩哔哩',
+  groups: [
+    {
+      key: 0,
+      name: '直播-游戏卡片-关闭',
+      rules: {
+        matches: '[vid="live_game_card_close"]',
+      },
+      activityIds: ['com.bilibili.bililive.room.ui.roomv3.LiveRoomActivityV3'],
+    },
+  ],
+});
