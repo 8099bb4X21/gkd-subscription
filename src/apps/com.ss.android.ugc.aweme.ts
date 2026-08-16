c35fa42e2cf592c98735f233535165c4cad5abd5
8099bb4X21 <hydra1018@gmail.com>
2026-08-16 12:59:35 +0800

    feat: 抖音新增游戏推广卡片关闭规则

---
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -0,0 +1,20 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.ss.android.ugc.aweme',
+  name: '抖音',
+  groups: [
+    {
+      key: 0,
+      name: '游戏推广卡片-关闭',
+      fastQuery: true,
+      rules: {
+        matches:
+          '@View[clickable=true][width<=101][height<=101] <<n [id="m.l.live.plugin:id/lynx_game_promote_card_container"]',
+        action: 'click',
+      },
+      activityIds: ['com.ss.android.ugc.aweme.live.LivePlayActivity'],
+      snapshotUrls: ['https://i.gkd.li/i/1786616809712'],
+    },
+  ],
+});
